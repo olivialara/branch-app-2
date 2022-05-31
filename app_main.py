7317d75ad35f9c82e6b79c23da38e6e9dfd2909c
Olivia Lara <oliviamarielara@gmail.com>
2022-05-30 19:39:29 -0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -4,7 +4,8 @@ import pandas as pd
 from PIL import Image
 from app_functions import *
 
-st.title('Implementing Laplace Mechanism & Kolmogorov-Smirnov Test')
+st.title('Laplace Mechanism')
+st.subtitle('Testing Varying Data Sizes, Ranges, Distributions, and Epsilon Values')
 range_number = st.number_input('Select a size for the dataset:', min_value=0, max_value=1_000_000, value=10_000)
 #st.write('Current size:', range_number)
 
